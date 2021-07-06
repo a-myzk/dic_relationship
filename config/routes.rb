bde0a24144bdf5224a55b220b013a452c5825035
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-07-06 09:41:26 +0900

    [Add]フォローユーザー一覧ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show]
 end
