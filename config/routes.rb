4e5103e3f87693208d8970d0916e6a41b56df258
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-07-06 09:51:20 +0900

    [Update]フォローボタン設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'sessions/new'
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :new, :create, :show]
+  resources :relationships, only: [:create, :destroy]
 end
